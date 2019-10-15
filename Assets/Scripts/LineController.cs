2c917217cee591118032a95b924c66826c4a0d4d
ssBandit <nirool358@gmail.com>
2019-10-15 20:49:28 +0300

    some minor changes lol

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    public LayerMask mask;
+
+    LineRenderer line;
+    Vector3 previousPoint = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (line.positionCount < 2) return;
+        RaycastHit hit;
+        if (Physics.Linecast(line.GetPosition(line.positionCount-2), line.GetPosition(line.positionCount-1), out hit, mask))
+        {
+            line.positionCount = 1;
+            Debug.Log("Line Snapped");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject anchor;
+
+    [Header("Look")]
+    public Camera mainCamera;
+    public float mouseSensitivity = 2;
+    public float upDownRange;
+    public float snappiness = 1;
+    public bool lockMouseMovement = false;
+    [Header("Movement")]
+    public float speed = 5;
+
+    Rigidbody rb;
+    float rotX;
+    float rotY;
+    float xVelocity;
+    float yVelocity;
+
+    Vector3 ninty = new Vector3(90, 0, 0);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        DrawLine();
+        line.SetPosition(line.positionCount-1, transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        Look();
+        Move();
+    }
+
+    void Look()
+    {
+        rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        xVelocity = Mathf.Lerp(xVelocity, rotX, snappiness * Time.deltaTime);
+        yVelocity = Mathf.Lerp(yVelocity, rotY, snappiness * Time.deltaTime);
+
+        if (!lockMouseMovement)
+        {
+            //RotY
+            rotY = Mathf.Clamp(rotY, -upDownRange, upDownRange);
+            mainCamera.transform.localRotation = Quaternion.Euler(yVelocity, 0, 0);
+
+            //RotX
+            transform.Rotate(0, xVelocity, 0);
+        }
+    }
+
+    void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * speed) + (transform.right * Input.GetAxis("Horizontal") * speed));
+    }
+
+    void DrawLine()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, 1))
+            {
+                Instantiate(anchor, hit.point, Quaternion.Euler(hit.normal + ninty));
+                line.SetPosition(line.positionCount - 1, hit.point);
+                line.positionCount++;
+            }
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (line.positionCount > 1)
+            {
+                line.positionCount--;
+            }
+        }
+    }
+}
